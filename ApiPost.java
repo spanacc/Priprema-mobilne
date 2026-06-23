ee1ce34f34834c7cf2478f6725de7e1871573ea8
Veljko Stojanovic <163127021+spanacc@users.noreply.github.com>
2026-06-23 18:36:17 +0200

    Remove unnecessary lines from ApiPost.java

---
+++ ApiPost.java
@@ -0,0 +1,14 @@
+package com.example.kolokvijum2;
+
+public class ApiPost {
+
+    private int id;
+    private String title;
+    private String body;
+    private int userId;
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getBody() { return body; }
+    public int getUserId() { return userId; }
+}
